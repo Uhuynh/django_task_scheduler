bb9b5ffb5f866687e782490a46f6b87211484551
uhuynh <Huy!4101>
2022-09-18 15:47:10 +0200

    validate request arg

--- app/views.py
+++ app/views.py
@@ -2,10 +2,11 @@ import datetime
 import pytz
 
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from rest_framework.decorators import api_view
 
 from app.tasks import company_name_bulk_create
 
@@ -24,8 +25,11 @@ def submit_celery_task(request):
     if request.method == 'POST':
         count = int(request.data['count'])
         execution_time = datetime.datetime.fromisoformat(request.data['execution_time']).astimezone(tz=pytz.utc).replace(tzinfo=None)
-        task = company_name_bulk_create.apply_async(
-            args=[count],
-            eta=execution_time
-        )
-    return JsonResponse({"task_id": task.id}, status=202)
+        if execution_time > datetime.datetime.utcnow():
+            task = company_name_bulk_create.apply_async(
+                args=[count],
+                eta=execution_time
+            )
+            return JsonResponse({"task_id": task.id}, status=202)
+        else:
+            return JsonResponse({}, status=400)
